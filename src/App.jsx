035bda979efa411878169961decb660be716bcb7
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2023-01-09 20:50:40 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './App.css';
+import moonIcon from './images/icon-moon.svg';
 
 function App() {
   return (
-    <div>
-      <h1>Todo App</h1>
+    <div className="app">
+      <header>
+        <h1>TODO</h1>
+        <img src={moonIcon} alt="moon-icon" />
+      </header>
     </div>
   );
 }
